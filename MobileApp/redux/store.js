29a02b48766ac82ccfc435c264f8ce5a6490732c
mnabeelawais <muhammadnabeelawais@gmail.com>
2026-03-09 11:54:07 +0500

    Update Store of MobileApp

--- MobileApp/redux/store.js
+++ MobileApp/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./api/authApi";
 import { reviewsApi } from "./api/reviewsApi";
+import { businessItemsApi } from "./api/businessItemsApi";
+import { itineraryApi } from "./api/itineraryApi";
+import { tripsApi } from "./api/tripsApi";
 import authReducer from "./slices/authSlice";
 import themeReducer from "./slices/themeSlice";
 
@@ -8,9 +11,17 @@ export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [reviewsApi.reducerPath]: reviewsApi.reducer,
+    [businessItemsApi.reducerPath]: businessItemsApi.reducer,
+    [itineraryApi.reducerPath]: itineraryApi.reducer,
+    [tripsApi.reducerPath]: tripsApi.reducer,
     auth: authReducer,
     theme: themeReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, reviewsApi.middleware),
+    getDefaultMiddleware()
+      .concat(authApi.middleware)
+      .concat(reviewsApi.middleware)
+      .concat(businessItemsApi.middleware)
+      .concat(itineraryApi.middleware)
+      .concat(tripsApi.middleware),
 });
