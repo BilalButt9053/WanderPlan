cbf3becf1cfce6f9c558736723f86c27f1f1041e
mnabeelawais <muhammadnabeelawais@gmail.com>
2026-04-06 12:49:18 +0500

    update admin router

--- Server/router/admin-router.js
+++ Server/router/admin-router.js
@@ -2,13 +2,21 @@ const express = require("express");
 const admin_controller = require("../controllers/admin-controllers");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth-middleware");
-const adminMiddleware=require("../middleware/admin-middleware")
+const adminMiddleware = require("../middleware/admin-middleware");
 
+// Apply middleware to all routes
+router.use(authMiddleware);
+router.use(adminMiddleware);
 
-router.route('/users').get(authMiddleware,adminMiddleware,admin_controller.getAllUser);
-router.route('/users/delete/:id').delete(authMiddleware,adminMiddleware,admin_controller.deleteUserById);
-router.route('/users/:id').get(authMiddleware,adminMiddleware,admin_controller.GetUserById);
-router.route('/users/update/:id').patch(authMiddleware,adminMiddleware,admin_controller.UpdateUserById);
-router.route('/make-admin').post(authMiddleware,adminMiddleware,admin_controller.makeUserAdmin);
+// User management routes
+router.get('/users', admin_controller.getAllUser);
+router.get('/users/list', admin_controller.getUsersList);
+router.get('/users/:id', admin_controller.GetUserById);
+router.get('/users/:id/details', admin_controller.getUserDetails);
+router.delete('/users/delete/:id', admin_controller.deleteUserById);
+router.patch('/users/update/:id', admin_controller.UpdateUserById);
+router.put('/users/:id/block', admin_controller.blockUser);
+router.put('/users/:id/unblock', admin_controller.unblockUser);
+router.post('/make-admin', admin_controller.makeUserAdmin);
 
 module.exports = router;
