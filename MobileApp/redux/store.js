5133d2e91adeb8f9a4e4b1822c5ef3afe3035c64
BilalButt9053 <bilalbutt6986@gmail.com>
2026-04-06 12:14:36 +0500

    update mobile store.js

--- MobileApp/redux/store.js
+++ MobileApp/redux/store.js
@@ -5,10 +5,13 @@ import { businessItemsApi } from "./api/businessItemsApi";
 import { itineraryApi } from "./api/itineraryApi";
 import { tripsApi } from "./api/tripsApi";
 import { complaintsApi } from "./api/complaintsApi";
+import { userProfileApi } from "./api/userProfileApi";
+import { placesApi } from "./api/placesApi";
 
 
 import authReducer from "./slices/authSlice";
 import themeReducer from "./slices/themeSlice";
+import tripsReducer from "./slices/tripsSlice";
 
 export const store = configureStore({
   reducer: {
@@ -18,8 +21,11 @@ export const store = configureStore({
     [itineraryApi.reducerPath]: itineraryApi.reducer,
     [tripsApi.reducerPath]: tripsApi.reducer,
     [complaintsApi.reducerPath]: complaintsApi.reducer,
+    [userProfileApi.reducerPath]: userProfileApi.reducer,
+    [placesApi.reducerPath]: placesApi.reducer,
     auth: authReducer,
     theme: themeReducer,
+    trips: tripsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
@@ -28,5 +34,7 @@ export const store = configureStore({
       .concat(businessItemsApi.middleware)
       .concat(itineraryApi.middleware)
       .concat(tripsApi.middleware)
-      .concat(complaintsApi.middleware),
+      .concat(complaintsApi.middleware)
+      .concat(userProfileApi.middleware)
+      .concat(placesApi.middleware),
 });
